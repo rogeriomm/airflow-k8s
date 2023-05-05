b5997ad6990851a22d379edac0f445a933956001
rogeriomm <rogermm@gmail.com>
2023-05-05 18:25:28 -0300

    Uses Airflow Helm char(User community), update README

--- build.mk
+++ build.mk
@@ -1,2 +1,2 @@
-Prj([Docker(name="airflow")],
+Prj([Docker(name="airflow", version="2.3.5", parm={"AIRFLOW_VERSION": "2.5.3-python3.8"})],
      "docker-airflow")
--- scripts/clean-pv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-sudo -k
-
-minikube --node=cluster2     ssh "sudo rm -rf /data/standard-storage/*"
-minikube --node=cluster2-m02 ssh "sudo rm -rf /data/standard-storage/*"
-minikube --node=cluster2-m03 ssh "sudo rm -rf /data/standard-storage/*"
