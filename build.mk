2fa047eee514110b046bd306b852d8012259ce4e
Rogerio Machado <rogermm@gmail.com>
2023-09-06 12:38:23 -0300

    Update to Airflow 2.7.1-rc2

--- build.mk
+++ build.mk
@@ -1,2 +1,5 @@
-Prj([Docker(name="airflow", version="2.7.0", parm={"AIRFLOW_VERSION": "2.7.0-python3.10"})],
-     "docker-airflow")
+Prj([
+       Docker(name="airflow", version="2.6.3-10", parm={"AIRFLOW_VERSION": "2.6.3-python3.10"}),
+       Docker(name="airflow", version="2.7.1-10", parm={"AIRFLOW_VERSION": "2.7.1rc2-python3.10"})
+    ],
+    "docker-airflow")
