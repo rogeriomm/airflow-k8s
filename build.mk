80198f6092220bfa456b5ec9fb0f570314bd04ae
Rogerio Machado <rogermm@gmail.com>
2023-09-20 08:44:45 -0300

    Bump Airflow version

--- build.mk
+++ build.mk
@@ -1,5 +1,5 @@
 Prj([
        Docker(name="airflow", version="2.6.3-10", parm={"AIRFLOW_VERSION": "2.6.3-python3.10"}),
-       Docker(name="airflow", version="2.7.1-10", parm={"AIRFLOW_VERSION": "2.7.1rc2-python3.10"})
+       Docker(name="airflow", version="2.7.1-10", parm={"AIRFLOW_VERSION": "2.7.1-python3.10"})
     ],
     "docker-airflow")
